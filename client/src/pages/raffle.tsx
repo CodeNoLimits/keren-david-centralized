67966a8cf3ad0ba0ded2f14264465245cb9c713b
CodeNoLimits <codenolimits@gmail.com>
2025-11-03 16:02:43 +0200

    feat: add raffle page placeholder

---
+++ client/src/pages/raffle.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Raffle = () => {
+  return (
+    <div className="p-4">
+      <h1>Loterie</h1>
+      <p>La page de loterie est en construction. Revenez bientôt!</p>
+    </div>
+  );
+};
+
+export default Raffle;
